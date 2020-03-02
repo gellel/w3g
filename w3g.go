2e068a56c3f62e498f0526c9950a06af0f6aac2f
gellel <lindsay.gelle@gmail.com>
2020-03-02 20:24:48 +1100

    Added to w3g.go

--- w3g.go
+++ w3g.go
@@ -27,6 +27,20 @@ const AcceptPatch string = "Accept-Patch"
 // AcceptRanges response HTTP header is a marker used by the server to advertise its support of partial requests.
 const AcceptRanges string = "Accept-Ranges"
 
-// AccessControlAllowCredentials response header tells browsers whether to expose the response to frontend JavaScript code
+// AccessControlAllowCredentials response HTTP header tells browsers whether to expose the response to frontend JavaScript code
 // when the request's credentials mode is include.
 const AccessControlAllowCredentials string = "Access-Control-Allow-Credentials"
+
+// AccessControlAllowHeaders response HTTP header is used in response to a preflight request which includes the
+// Access-Control-Request-Headers to indicate which HTTP headers can be used during the actual request.
+const AccessControlAllowHeaders string = "Access-Control-Allow-Headers"
+
+// AccessControlAllowMethod response HTTP header specifies the method or methods allowed when accessing
+// the resource in response to a preflight request.
+const AccessControlAllowMethod string = "Access-Control-Allow-Method"
+
+// AccessControlAllowOrigin response header indicates whether the response can be shared with requesting code from the given origin.
+const AccessControlAllowOrigin string = "Access-Control-Allow-Origin"
+
+// AccessControlExposeHeaders response header indicates which headers can be exposed as part of the response by listing their names.
+const AccessControlExposeHeaders string = "Access-Control-Expose-Headers"
