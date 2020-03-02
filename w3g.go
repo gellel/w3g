075577ba79298b9539fcfaa0e5c018d77b841850
gellel <lindsay.gelle@gmail.com>
2020-03-02 20:12:15 +1100

    Added w3g.go

---
+++ w3g.go
@@ -0,0 +1,7 @@
+package w3g
+
+// Accept request HTTP header advertises which content types, expressed as MIME types, the client is able to understand.
+const Accept string = "Accept"
+
+// AcceptCH header is set by the server to specify which Client Hints headers client should include in subsequent requests.
+const AcceptCH string = "Accept-CH"
