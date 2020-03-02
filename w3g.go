1365864f964ea37d03603b2f90a55ccbee7dc7e5
gellel <lindsay.gelle@gmail.com>
2020-03-02 20:15:47 +1100

    Added to w3g.go

--- w3g.go
+++ w3g.go
@@ -3,5 +3,16 @@ package w3g
 // Accept request HTTP header advertises which content types, expressed as MIME types, the client is able to understand.
 const Accept string = "Accept"
 
-// AcceptCH header is set by the server to specify which Client Hints headers client should include in subsequent requests.
+// AcceptCH HTTP header is set by the server to specify which Client Hints headers client should include in subsequent requests.
 const AcceptCH string = "Accept-CH"
+
+// AcceptCHLifetime HTTP header is set by the server to specify the persistence of Accept-CH header value
+// that specifies for which Client Hints headers client should include in subsequent requests.
+const AcceptCHLifetime string = "Accept-CH-Lifetime"
+
+// AcceptCharset request HTTP header advertises which character encodings the client understands.
+const AcceptCharset string = "Accept-Charset"
+
+// AcceptEncoding request HTTP header advertises which content encoding,
+// usually a compression algorithm, the client is able to understand.
+const AcceptEncoding string = "Accept-Encoding"
